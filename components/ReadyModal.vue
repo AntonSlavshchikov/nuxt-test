60630b154ed73e04186a544bd7e995d34b834ba7
AntonSlav <lozzyklingers@gmail.com>
2022-06-15 16:01:12 +0600

    order

---
+++ components/ReadyModal.vue
@@ -0,0 +1,59 @@
+<template>
+     <modal
+    name="ready-modal"
+    transition="pop-out"
+    height="100%"
+    width="100%"
+    :max-width="400"
+    :max-height="250"
+    :adaptive="true"
+    :scrollable="true"
+    :pivot-y="0.5"
+    :reset="true"
+    classes="v--modal-customer-cart"
+ >
+    <div class="ready__modal">
+        <div class="ready__modal__content">
+            <h1 class="ready__modal__text">Заказ оформлен!</h1>
+            <NuxtLink to='/' class="ready__btn">OK</NuxtLink>
+        </div>
+       
+    </div>
+  </modal>
+</template>
+
+<script>
+    export default {
+        name: 'ReadyModal'
+    }
+</script>
+
+<style lang="scss" scoped>
+    .ready__modal{
+        width: 100%;
+        height: 100%;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+
+    .ready__modal__content{
+        text-align: center;
+    }
+
+    .ready__modal__text{
+        margin-bottom: 20px;
+    }
+
+    .ready__btn{
+        background: teal;
+        border: none;
+        padding: 10px 20px;
+        color: #fff;
+        font-size: 14px;
+        text-decoration: none;
+
+        border-radius: 5px;
+    }
+</style>
---
+++ pages/order.vue
@@ -0,0 +1,123 @@
+<template>
+    <div class="container">
+        <ReadyModal/>
+        <div class="order__page">
+            <h3 class="page__header">Оформление заказа</h3>
+
+            <div class="order__list">
+                <div class="order__item" v-for="product in $store.state.basket" :key="product.item.id">
+                    <img src="https://via.placeholder.com/80" alt="" class="order__img">
+                    <p class="order__text">{{product.item.name}}</p>
+                    <p class="order__text">{{product.item.price}}</p>
+                    <p class="order__text">{{product.item.name}}</p>
+                    <p class="order__text">x{{product.col}}</p>
+                </div>
+            </div>
+
+            <h4>Общая стоимость: {{$store.state.total}} руб.</h4>
+
+            <form class="order__form" action="">
+                <h3>Заполните данные</h3>
+                <input class="order__input" placeholder="ФИО" type="text">
+                <input class="order__input" placeholder="Серия и номер паспорта" type="text">
+                <input class="order__input" placeholder="Адрес доставки (город, улица, дом)" type="text">
+                <input class="order__input" placeholder="Номер телефона" type="text">
+                <button class="order__btn" @click.prevent="$modal.show('ready-modal')">Оформить</button>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'order',
+        mounted() {
+            this.$modal.hide('customer-cart')
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .order__page{
+        margin-top: 10px;
+    }
+
+    .order__list{
+        padding: 30px;
+    }
+
+    .order__item{
+        display: flex;
+        align-items: center;
+
+        margin-bottom: 15px;
+        padding-bottom: 5px;
+
+        border-bottom: 1px solid #000;
+    }
+
+    .order__img{
+        margin-right: 15px;
+    }
+
+    .order__text{
+        margin-right: 15px;
+        
+        font-size: 16px;
+        font-weight: 700;
+
+        &:last-child{
+            margin-right: 0;
+        }
+    }
+
+
+
+    .order__form{
+        background: #fff;
+        padding: 30px;
+        width: 50%;
+        margin-top: 20px;
+
+        border-radius: 20px;
+
+        box-shadow: 1px 1px 5px #000;
+    }
+
+    .order__input{
+        display: block;
+
+        border: none;
+        background: #eeeeee;
+        border-radius: 5px;
+        padding: 10px;
+
+        font-size: 18px;
+        color: #222831;
+        margin-bottom: 8px;
+
+        width: 100%;
+    }
+
+    .order__btn{
+        height: 30px;
+        border: none;
+        background: #393E46;
+        color: #fff;
+        padding: 8px;
+        font-size: 16px;
+        font-weight: 700;
+        text-transform: uppercase;
+        cursor: pointer;
+        border-radius: 5px;
+
+        text-decoration: none;
+
+        transition: all .2s linear;
+
+        &:hover{
+            background: lighten($color: #393E46, $amount: 10);
+        }
+    }
+
+</style>
